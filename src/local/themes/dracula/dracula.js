e80099d89722a47c9794dba6c3ad64670983d9dd
Ryan C Koch <ryanckoch@gmail.com>
2019-01-29 11:55:36 -0800

    base16 shell and blinksh themes

---
+++ src/local/themes/dracula/dracula.js
@@ -0,0 +1,47 @@
+// Base16 Dracula
+// Scheme: Mike Barkmin (http://github.com/mikebarkmin) based on Dracula Theme (http://github.com/dracula)
+
+base00 = '#282936';
+base01 = '#3a3c4e';
+base02 = '#626483';
+base03 = '#4d4f68';
+base04 = '#62d6e8'; //blue
+base05 = '#e9e9f4';
+base06 = '#f1f2f8';
+base07 = '#f7f7fb';
+base08 = '#ea51b2'; //pink
+base09 = '#b45bcf'; //purple
+base0A = '#ebff87'; //green
+base0B = '#00f769'; //yellow
+base0C = '#a1efe4'; //light blue
+base0D = '#62d6e8';
+base0E = '#b45bcf';
+base0F = '#00f769';
+
+t.prefs_.set('color-palette-overrides', 
+                        [base00,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base05,
+                        base03,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base07,
+                        base09,
+                        base0F,
+                        base01,
+                        base02,
+                        base04,
+                        base06]);
+
+t.prefs_.set('cursor-color', "rgba(233, 233, 244, 0.5)");
+t.prefs_.set('foreground-color', base05);
+t.prefs_.set('background-color', base00);
