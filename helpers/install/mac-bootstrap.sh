ad5e1ea2581fff5e6cf935160087d9f406d30166
Lilith Moon Cohen <lilithmooncohen@gmail.com>
2021-01-04 16:52:58 -0700

    latest config

--- helpers/install/mac-bootstrap.sh
+++ helpers/install/mac-bootstrap.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-xcode-select --install
-sudo easy_install install pip
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+set -e
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# install xcode if not installed
+xcode-select --install || true
+
+# install brew if not installed
+if ! command -v brew >/dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# install python via homebrew if not installed
+if ! brew ls --versions python >/dev/null 2>&1; then
+    brew install python
+fi
+
+# install ansible via pip if not installed
+if ! command -v ansible >/dev/null 2>&1; then
+    pip3 -u ansible
+fi
+
+# run ansible playbook
+ansible-playbook ${SCRIPTDIR}/mac.yml
