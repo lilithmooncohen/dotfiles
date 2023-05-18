9c5f20c09538635984ba09c0d425e99a55b37040
Lily Cohen <lilithmooncohen@gmail.com>
2023-05-18 11:13:29 -0600

    make install easier

--- helpers/install/mac-bootstrap.sh
+++ helpers/install/mac-bootstrap.sh
@@ -10,6 +10,15 @@ xcode-select --install || true
 # install brew if not installed
 if ! command -v brew >/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install libjpeg openblas openssl readline sqlite3 xz zlib cmake
+fi
+
+# install ansible-playboook if not installed
+if ! command -v ansible-playbook >/dev/null 2>&1; then
+    asdf plugin add github.com/danhper/asdf-python.git
+    asdf install python
+    source ~/.profile
+    pip install ansible
 fi
 
 # run ansible playbook
